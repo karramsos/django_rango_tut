043dfbdf7e9ecb035c2671e44bad013babcd13ca
Sukhvinder Singh <karramsos@gmail.com>
2019-08-28 17:42:18 +0200

    finished exercise chapter 6 - added views of pages

--- rango/views.py
+++ rango/views.py
@@ -13,6 +13,8 @@ def index(request):
     context_dict = {}
     context_dict['boldmessage'] = 'Crunchy, creamy, cookie, candy, cupcake!'
     context_dict['categories'] = category_list
+    page_list = Page.objects.order_by('-views')[:5]
+    context_dict['pages'] = page_list
 
     # Render the response and send it back!
     return render(request, 'rango/index.html', context_dict)
---
+++ test_cat.py
@@ -0,0 +1,2 @@
+cats = {'Python': {'pages': python_pages, 'views': 128, 'likes': 64},'Django': {'pages': django_pages, 'views': 64, 'likes': 32},'Other Frameworks': {'pages': other_pages, 'views': 32, 'likes': 16} }
+
